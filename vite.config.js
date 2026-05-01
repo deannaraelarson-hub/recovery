ae7c7ad14ca3febdf6ccdb507e7d486c8fcb5304
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-05-01 12:49:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  optimizeDeps: {
+    include: ['@reown/appkit', '@reown/appkit-adapter-wagmi', 'wagmi', 'viem']
   },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ethers: ['ethers']
-        }
-      }
+    commonjsOptions: {
+      include: [/node_modules/]
     }
-  },
-  optimizeDeps: {
-    include: ['ethers']
   }
 })
